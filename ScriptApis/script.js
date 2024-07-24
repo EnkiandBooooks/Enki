11fc2f85f3ca1b448bf0003b9bede6b8308a2c05
Merino-Magrina Max <max.merino.ext@inetum.com>
2024-07-24 09:06:58 +0200

    Búsqueda de más de un libro, método abecedario

--- ScriptApis/script.js
+++ ScriptApis/script.js
@@ -8,34 +8,42 @@ const mongoClient = new MongoClient(mongoUri);
 
 const searchTerms = ['fiction', 'nonfiction', 'mystery', 'fantasy', 'history', 'science', 'romance', 'adventure'];
 
-function getRandomSearchTerm() {
+function recogerQuery() {
   return searchTerms[Math.floor(Math.random() * searchTerms.length)];
 }
 
-async function fetchRandomBook() {
-  const randomTerm = getRandomSearchTerm();
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${randomTerm}&maxResults=1&key=${googleBooksApiKey}`;
+async function recogerLibros() {
+  const randomTerm = "a";
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${randomTerm}&maxResults=5&key=${googleBooksApiKey}`;
   const response = await axios.get(url);
-  const book = response.data.items ? response.data.items[0] : null;
-  return book ? {
-    title: book.volumeInfo.title,
-    authors: book.volumeInfo.authors,
-    publishedDate: book.volumeInfo.publishedDate,
-    description: book.volumeInfo.description,
-    pageCount: book.volumeInfo.pageCount,
-    categories: book.volumeInfo.categories,
-    thumbnail: book.volumeInfo.imageLinks?.thumbnail,
-  } : null;
+  const apiBooks = response.data.items ? response.data.items : null;
+  let i = 0;
+  let books = []
+  
+  for(book of apiBooks){
+    books[i] = {
+      title: book.volumeInfo.title,
+      authors: book.volumeInfo.authors,
+      publishedDate: book.volumeInfo.publishedDate,
+      description: book.volumeInfo.description,
+      pageCount: book.volumeInfo.pageCount,
+      categories: book.volumeInfo.categories,
+      thumbnail: book.volumeInfo.imageLinks?.thumbnail,
+    };
+    i++;
+  }
+  console.log(books)
+  return books;  
 }
 
 
 
-async function insertBookIntoDb(book) {
+async function moverLibroDB(books) {
   try {
     await mongoClient.connect();
     const database = mongoClient.db('applibros');
     const collection = database.collection('obras');
-    await collection.insertOne(book);
+    await collection.insertMany(books);
   } catch(error){
     console.log(error);
   } 
@@ -46,14 +54,15 @@ async function insertBookIntoDb(book) {
 
 async function anadirLibro(){
   try {
-    const book = await fetchRandomBook();
+    const book = await recogerLibros();
     if (!book) {
       console.log('No se pudo encontrar un libro.');
-      return;
-    }
-    await insertBookIntoDb(book);
     
-    console.log('Libro aleatorio insertado en la colección obras.');
+    }else{
+      await moverLibroDB(book);
+      console.log('Libro aleatorio insertado en la colección obras.');
+    }
+    
   } catch (error) {
     console.error(error);
     console.log('Ocurrió un error al insertar el libro.');
