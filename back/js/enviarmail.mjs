2b291d7f45eb63ab41bf0e9b27927e5849ad99de
Robles-Royuela Cesar <cesar.robles.ext@inetum.com>
2024-07-29 08:36:56 +0200

    cambios importación

--- back/js/enviarmail.mjs
+++ back/js/enviarmail.mjs
@@ -1,11 +1,15 @@
-const mail = require("./index.js")
-
-const transporter = require("./transporter"); //creamos la constante transporter y importamos la info de ./transporter
+import transporter from "./transporter.mjs";
+function getRandom(min,max) { //esta funcion es para crear el random
+    return Math.random() * (max - min) + min;
+  }
+  
+  let random = getRandom(100000,999999) //numero random de 6 digitos
+  random = Math.trunc(random) //lo truncamos para que sea un entero
 
 const EnviarMail = (mail) => {
 
     transporter.sendMail({ //creamos el operador que enviara el mail
-        from: "cesarrobresroyuela@gmail.com", // el correo que envía
+        from: process.env.EMAIL_USER, // el correo que envía
         to: `${mail}`, // a quién se lo enviamos
         subject: `Correo de verificación`, // esto seria el encabezado
         text: `Esto es el código de verificación que necesitarás: \n ${random}` //random es el numero, esto seria el texto, y usamos comillas invertidas ``
@@ -19,16 +23,9 @@ const EnviarMail = (mail) => {
     });
     
 }
-function getRandom(min,max) { //esta funcion es para crear el random
-    return Math.random() * (max - min) + min;
-  }
-  
-  let random = getRandom(100000,999999) //numero random de 6 digitos
-  random = Math.trunc(random) //lo truncamos para que sea un entero
-  exports.EnviarMail = EnviarMail;
-  exports.random = random; 
   console.log(Math.trunc(random)) //imprime el numero que vamos a enviar.
 
+  export { EnviarMail, random}; //exportamos el enviar mail
 
 
 
