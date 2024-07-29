414061ab2d054f30fc37b3616095caa65564ea28
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-07-29 12:21:09 +0200

    Arreglado formato de envios del código

--- back/js/enviarmail.js
+++ back/js/enviarmail.js
@@ -4,15 +4,12 @@ export const EnviarMail = (mail, random) => {
 
     transporter.sendMail({ //creamos el operador que enviara el mail
         from: process.env.EMAIL_USER, // el correo que envía
-        to: `${mail}`, // a quién se lo enviamos
+        to: mail, // a quién se lo enviamos
         subject: `Correo de verificación`, // esto seria el encabezado
         text: `Esto es el código de verificación que necesitarás: \n ${random}` //random es el numero, esto seria el texto, y usamos comillas invertidas ``
     }, 
-    
     (error, info) => { //info por si nos da error, sobretodo utilizado hasta que el codigo funcionase
-        if (error) {
-            return console.log(error);
-        }
+        if (error) return console.log(error);
         console.log('Message sent: %s', info.messageId);
     });    
 }
--- back/js/random.js
+++ back/js/random.js
@@ -1,3 +1,3 @@
 export function getNumRandom(min = 100000, max = 999999) { //esta funcion es para crear el random
     return Math.trunc(Math.random() * (max - min) + min);
-}
+};
