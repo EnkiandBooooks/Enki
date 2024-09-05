cbbe2cad3d85e4064aea1f6dc5673941440b665b
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-09-05 09:22:56 +0200

    Solucionado problemas subir datos desde front a la bd

--- back/js/models/mongodb/register.js
+++ back/js/models/mongodb/register.js
@@ -1,25 +1,8 @@
-import { MongoClient } from 'mongodb';
-import 'dotenv/config';
-
-const client = new MongoClient(process.env.MONGO_URI);
-
-async function connect () {
-    try {
-        await client.connect();
-
-        const database = client.db('applibros');
-        console.log("BASE DE DATOS CONECTADA!")
-        return database.collection('usuarios');
-        
-    } catch (error) {
-        console.error('Error al conectar a la base de datos.');
-        console.error(error);
-    }
-}
+import { connect } from "./connectBD.js";
 
 export class RegisterModel {
     static async insertarUsuario (nuevoUsuario){
-        const db = await connect();
+        const db = await connect('usuarios');
         const create = await db.insertOne(nuevoUsuario);
     }
 }
