4deffc418f8e8e324b45905419d52bb91f1327f2
Antonio Cobo <antonio.cobo.ext@inetum.com>
2024-09-05 08:21:50 +0200

    subo front

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -7,23 +7,37 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDivider } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common'; // importar siempre
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [Login1Component,RouterOutlet,RouterLink,MatButtonModule,MatIconModule,MatDivider,MatToolbarModule],
+  imports: [Login1Component,RouterOutlet,RouterLink,MatButtonModule,MatIconModule,MatDivider,MatToolbarModule,CommonModule
+    
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
 
 export class HeaderComponent {
+  title = 'enkiweb';
+  showHeaderAndBody: boolean = true; // Variable para controlar la visibilidad del header y el body
 
-  // constructor(private router: Router) { }
+  constructor(private router: Router) {
+    // Suscribirse a los eventos de navegación del router
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Ocultar el header y body si la ruta es '/login1', '/login2' o '/login3'
+      this.showHeaderAndBody = !['/login1', '/login2', '/login3'].includes(event.url);
+    });
+  }
 
-  // onClick() {
-  //   console.log();
-  //   this.router.navigate(['/login1']); // Usar ruta absoluta
-  // }
-
+  onClick() {
+    this.router.navigate(['/login1']); // Usar ruta absoluta
+  }
 }
+
