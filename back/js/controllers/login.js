61ddc5965215ef38426db487ca0cd387959c9b28
Antonio Cobo <antonio.cobo.ext@inetum.com>
2024-09-19 16:06:35 +0200

    verificador de login

--- back/js/controllers/login.js
+++ back/js/controllers/login.js
@@ -3,17 +3,17 @@ import bcrypt from 'bcrypt';
 
 export class loginControler{
     static async loginUsuario(req, res){
-        usr = req.body.usr
-        pwd = req.body.pwd
+        const usr = req.body.usr
+        const pwd = req.body.pwd
 
         try{
-            user = RegisterModel.buscarUsuario({userName : usr})
+            const user = await RegisterModel.buscarUsuario({userName : usr})
             console.log(user)
             const isValid = await bcrypt.compare(pwd, user.password)
             console.log("Usuario loggeado.")
-            res.status(200).json({resultado: "Usuario Correcto"});
+            return res.status(200).json({resultado: "Usuario Correcto"});
         } catch(err) {
-            res.status(200).json({resultado: "Usuario no existe"});
+            return res.status(200).json({resultado: "Usuario no existe"});
         }
     }
 }
