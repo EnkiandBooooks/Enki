5a5f6bf3cff6a13d9a1a600817a49b84750f04f2
Antonio Cobo <antonio.cobo.ext@inetum.com>
2024-09-20 11:55:43 +0200

    subo cambios pass

---
+++ front/src/app/usuarios/cambio-pass2/cambio-pass2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cambio-pass2',
+  standalone: true,
+  imports: [],
+  templateUrl: './cambio-pass2.component.html',
+  styleUrl: './cambio-pass2.component.css'
+})
+export class CambioPass2Component {
+
+}
