dfda9746049cfe363cd3edc31a36a333366354c3
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-09-21 12:59:28 +0200

    Hecha la parte del login del backend y comentada.

--- back/js/controllers/login.js
+++ back/js/controllers/login.js
@@ -1,19 +1,30 @@
 import { RegisterModel } from "../models/mongodb/register.js"
-import bcrypt from 'bcrypt';
+import { PasswdHashManager } from "../utils/passwdhash.js";
 
+
+/**
+ * Clase que gestiona las operaciones de login.
+ */
 export class LoginController{
+    /**
+     * Recibe los datos de usuario y contraseña, comprueba que el usuario existe en la base de datos y comprueba
+     * si la contraseña es la misma.
+     *
+     * @param {Object} req - Objeto de solicitud (Request) de Express, que contiene los datos de usuario y contraseña.
+     * @param {Object} res - Objeto de respuesta (Response) de Express.
+     * @returns {Promise<void>} - Responde con un mensaje de éxito o un error del servidor.
+     */
     static async loginUser(req, res){
-        const usr = req.body.usr
-        const pwd = req.body.pwd
-        
-        try{
-            const user = await RegisterModel.searchUser({userName : usr})
-            console.log(user)
-            const isValid = await bcrypt.compare(pwd, user.password)
-            console.log("Usuario loggeado.")
-            return res.status(200).json({resultado: "Usuario Correcto"});
-        } catch(err) {
-            return res.status(200).json({resultado: "Usuario no existe"});
+        const usr = req.body.usr;
+        const pwd = req.body.pwd;
+        const user = await RegisterModel.searchUser({username : usr});      // Hacemos una consulta del usuario.
+
+        if(user === null){          //Si no existe devolvemosn un 404.
+            res.status(404).json({"resultado": "Usuario no existe"});
         }
+
+        const isValid = PasswdHashManager.compareHash(pwd, user.password);      // Comprobamos si la contraseña es la misma.
+        // Depende de si es la misma contraseña envia un mensaje de error o de confirmación.
+        isValid ? res.status(200).json({resultado: "Usuario Correcto"}) : res.status(200).json({resultado: "Usuario no existe"});
     }
 }
