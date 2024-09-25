385be56d773da4fb7c2b9bee5e82ed6027f41b3c
Robles-Royuela Cesar <cesar.robles.ext@inetum.com>
2024-09-25 09:34:15 +0200

    Cambios 1 resetpswd

--- back/js/utils/enviarpswd.js
+++ back/js/utils/enviarpswd.js
@@ -1,5 +1,5 @@
 import transporter from "../config/transporter.js";
-
+import {  resetPswdController } from '../controllers/resetpswd.js';
 export const EnviarMailpswd = (mail) => {
 
     transporter.sendMail({      //creamos el operador que enviara el mail
@@ -10,7 +10,7 @@ export const EnviarMailpswd = (mail) => {
         html: ` <div style="width: 100%; max-width: 600px; margin: 0 auto; text-align: center; padding: 20px; border: 1px solid; border-radius: 8px;">
                     <img src="cid:logo@enki.com" style="display: block; margin: 0 auto; width: 300px; height: auto; border-radius: 8px ;" />
                     <h1>CAMBIO DE CONTRASEÑA</h1>
-                    <p>Para hacer el cambio, presione el siguiente <b>ENLACE</b>: <br/><br/> <h1>ENLACE QUE QUIERAS</h1></p>
+                    <p>Para hacer el cambio, presione el siguiente <b>ENLACE</b>: <br/><br/> <h1>${resetPswdController.recibirMailPswd()}</h1></p>
                 <div style="border-top: 1px solid #ddd; margin: 5px ;" />
                 <p> En caso de que usted no haya intentado cambiar la contraseña<b> alguien a intentado entrar en tu cuenta de Enki</b>. Te recomendamos restablecer tu contraseña.</p>
 
@@ -23,6 +23,6 @@ export const EnviarMailpswd = (mail) => {
                 cid: 'logo@enki.com'
             }
         ]
-
-
-        }, )}
+            }, )
+        console.log(temporaryUrl)
+        }
