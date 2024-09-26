85ffa9cc91c0fefbd080f14e86f03cc55d36cf69
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-09-26 12:43:15 +0200

    Arreglada las rutas backend del /resetpwd y creados Guards para rutas y enrutamiento

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -6,27 +6,24 @@ import { Login3Component } from './header/login3/login3.component';
 import { Login0Component } from './header/login0/login0.component';
 import { BodyComponent } from './body/body.component';
 import { PerfilComponent } from './usuarios/perfil/perfil.component';
-import { FormsModule } from '@angular/forms';
-
-import { MatToolbarModule } from '@angular/material/toolbar';  // Importar el módulo para la barra de herramientas
-import { MatButtonModule } from '@angular/material/button';    // Importar el módulo para los botones
 import { AppComponent } from './app.component';
 import { CambioPassComponent } from './usuarios/cambio-pass/cambio-pass.component';
 import { CambioPass3Component } from './usuarios/cambio-pass3/cambio-pass3.component';
 import { loginGuard } from './guards/login.guard';
 import { registerGuard } from './guards/register.guard';
-import { unsavedGuard } from './guards/unsaved.guard';
+import { notExitGuard } from './guards/not-exit.guard';
+import { notLoginGuard } from './guards/notLogin.guard';
 
 
 export const routes: Routes = [
     { path: 'inicio', component : AppComponent }, // Ruta principal que carga el Header
     { path: 'header', component: HeaderComponent }, // Ruta para el componente Login1
-    { path: 'login1', component: Login1Component, canActivate: [loginGuard] }, // Ruta para el componente Login2
-    { path: 'login2', component: Login2Component, canActivate: [loginGuard]}, // Ruta para el componente Login2
-    { path: 'login3', component: Login3Component, canActivate: [loginGuard], canDeactivate: [unsavedGuard] }, // Ruta para el component Login3
-    { path: 'login0', component: Login0Component, canActivate: [loginGuard] }, // Ruta para el component Login0
+    { path: 'login1', component: Login1Component, canActivate: [notLoginGuard] }, // Ruta para el componente Login2
+    { path: 'login2', component: Login2Component, canActivate: [registerGuard, loginGuard], canDeactivate: [notExitGuard] }, // Ruta para el componente Login2
+    { path: 'login3', component: Login3Component, canActivate: [registerGuard, loginGuard], canDeactivate: [notExitGuard] }, // Ruta para el component Login3
+    { path: 'login0', component: Login0Component, canActivate: [notLoginGuard] }, // Ruta para el component Login0
 
-    { path:'perfil', component:PerfilComponent, canActivate: [registerGuard] },// Ruta para el componente perfil
+    { path:'perfil', component:PerfilComponent, canActivate: [loginGuard] },// Ruta para el componente perfil
     { path:'resetPswd', component: CambioPassComponent }, // Ruta para el component cambioPass
     { path:'resetPswd3/:tokenPswd', component: CambioPass3Component }, // Ruta para el component  cambioPass3
     { path: 'body',component:BodyComponent},
