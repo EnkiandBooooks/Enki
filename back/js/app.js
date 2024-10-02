4e383fd886cdbd829ec8d64ef7cc1b26062aeb50
Robles-Royuela Cesar <cesar.robles.ext@inetum.com>
2024-10-02 11:27:58 +0200

    Save img in backend

--- back/js/app.js
+++ back/js/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import { usersRouter } from "./routes/users.js";
 import { booksRouter } from "./routes/books.js";
+import { cuttImgRouter } from "./routes/cutImg/cuttImg.js";
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,8 @@ app.use(express.json());
 
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
+app.use('/', cuttImgRouter);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port http://localhost:${process.env.PORT}`);
