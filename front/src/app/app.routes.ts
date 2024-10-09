608f4090e4e3f293548a377c9b4fb7f5cff04476
PolBuqueras <pol.buqueras.ext@inetum.com>
2024-10-09 12:46:05 +0200

    HomeDash carousel con grid

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { registerGuard } from './guards/register.guard';
 import { notExitGuard } from './guards/not-exit.guard';
 import { notLoginGuard } from './guards/notLogin.guard';
 import { DashboardComponent } from './header/dashboard/dashboard.component';
+import { HomedashComponent } from './header/homedash/homedash.component';
 
 
 export const routes: Routes = [
