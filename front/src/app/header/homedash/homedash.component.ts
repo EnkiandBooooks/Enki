b50a786002cf17f6ac78e833c030b4482d225f11
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-10-14 12:41:49 +0200

    Ya sacamos titulo y imagen(Mas o menos), de la BBDD, falta implementar Paginator en la biblioteca

--- front/src/app/header/homedash/homedash.component.ts
+++ front/src/app/header/homedash/homedash.component.ts
@@ -1,68 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
-
-interface Book {
-  id: number;
-  title: string;
-  imageUrl: string;
-}
+import { RestService } from '../../rest.service';
+import { ArrayType } from '@angular/compiler';
 
 @Component({
   selector: 'app-homedash',
   templateUrl: './homedash.component.html',
   styleUrls: ['./homedash.component.css'],
   standalone: true,
-  imports: [
-            CommonModule, MatCardModule,MatChipsModule,MatIcon
-            ]
+  imports: [CommonModule, MatCardModule,MatChipsModule,MatIcon]
 })
 export class HomedashComponent implements OnInit {
-  books: Book[] = [
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 1, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    { id: 2, title: 'Berserk', imageUrl: '/images/berserk.jpg'},
-    
-  ];
+  arrBooks = signal<any[]>([])
+  restService = inject(RestService);
 
   currentIndex = 0;
   cardWidth = 216; // 200px width + 16px margin-right
 
-  ngOnInit() {}
+  async ngOnInit() {
+    const allBooks = await this.restService.getBooks();
+    this.arrBooks.set(allBooks);
+  }
 
   get translateX(): string {
     return `translateX(${-this.currentIndex * this.cardWidth *5}px)`;
   }
 
   nextSlide() {
-    if (this.currentIndex < this.books.length - 5) {
+    if (this.currentIndex < 100) {
       this.currentIndex++;
     }
   }
