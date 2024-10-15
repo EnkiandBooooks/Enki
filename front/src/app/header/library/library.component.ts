2ce1fbfe37333ed708350ded54d7088cc6042f9e
Gerard Acosta Bazaga <gerard.acosta.ext@inetum.com>
2024-10-15 11:34:03 +0200

    Añadida funcionalidad de páginas en la biblioteca y simplificada la manera de guardar los libros, añadida libreria en angular

--- front/src/app/header/library/library.component.ts
+++ front/src/app/header/library/library.component.ts
@@ -3,33 +3,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import { RestService } from '../../rest.service';
-import { PageEvent, MatPaginatorModule, MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
-import { Subject } from 'rxjs';
-
-// TODO Conseguir enlazar el MatPaginator con los libros.
-@Injectable()
-export class MyCustomPaginatorIntl implements MatPaginatorIntl {
-  changes = new Subject<void>();
-
-  // For internationalization, the `$localize` function from
-  // the `@angular/localize` package can be used.
-  firstPageLabel = "First page"; //$localize`First page`;
-  itemsPerPageLabel = "Item in page"//$localize`Items per page:`;
-  lastPageLabel = "Last page";//$localize`Last page`;
-
-  // You can set labels to an arbitrary string too, or dynamically compute
-  // it through other third-party internationalization libraries.
-  nextPageLabel = "Next page";//'Next page';
-  previousPageLabel = "Previous page";//'Previous page';
-
-  getRangeLabel(page: number, pageSize: number, length: number): string {
-    if (length === 0) {
-      return "Pagina 1 de 1"; //$localize`Page 1 of 1`;
-    }
-    const amountPages = Math.ceil(length / pageSize);
-    return `Pagina ${page + 1} de ${amountPages}`;//$localize`Page ${page + 1} of ${amountPages}`;
-  }
-}
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-library',
@@ -37,23 +11,21 @@ export class MyCustomPaginatorIntl implements MatPaginatorIntl {
   imports: [MatGridListModule,
               CommonModule,
               MatCardModule,
-              MatPaginatorModule
+              NgxPaginationModule,
               ],
-  providers: [{provide: MatPaginatorIntl, useClass:MyCustomPaginatorIntl}],
+  providers: [],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
 })
 export class LibraryComponent{
-  arrBooks = signal<any[]>([])
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
+  p: number = 1;
+  books:any;
   restService = inject(RestService);
 
-  async ngOnInit() {
-    const allBooks = await this.restService.getBooks();
-    this.arrBooks.set(allBooks);
+  ngOnInit() {
+    this.restService.getBooks()
+      .subscribe((res: any) => {
+        this.books = res;
+    })
   }
-
-  // ngAfterViewInit() {
-  //   this.arrBooks.paginator = this.paginator;
-  // }
 }
