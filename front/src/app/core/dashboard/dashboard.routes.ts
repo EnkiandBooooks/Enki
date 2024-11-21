1599a94896ac40f135643fc1e1360773e93c1920
Robles-Royuela Cesar <cesar.robles.ext@inetum.com>
2024-11-21 08:32:12 +0100

    <feat> Visualizacion sin acabar de dos componentes a la vez

--- front/src/app/core/dashboard/dashboard.routes.ts
+++ front/src/app/core/dashboard/dashboard.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { CommentboxComponent } from "./components/workspace/commentbox/commentbox.component";
-
+import { WorkspaceComponent } from "./components/workspace/workspace.component";
 
 export const DASHBOARD_ROUTES: Routes = [
     { path: '', component: DashboardComponent },
-    { path:'commentbox',component:CommentboxComponent } 
+    { path:'commentbox',component:CommentboxComponent } ,
+    { path:'workspace',component:WorkspaceComponent } 
 ];
