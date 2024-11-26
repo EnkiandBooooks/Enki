79270a660e345ea70ee5a8cec10db414a3796d7c
pablocarvalho <pablo.carvalho.ext@inetum.com>
2024-11-26 08:56:41 +0100

    <feat> Nueva Timeline con HTML y CSS

--- front/src/app/core/dashboard/components/workspace/timeline/timeline.component.ts
+++ front/src/app/core/dashboard/components/workspace/timeline/timeline.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
   standalone: true, // Hace que el componente sea standalone
-  imports: [MatProgressBarModule] // Importa Angular Material ProgressBar directamente
+  imports: [] // Importa Angular Material ProgressBar directamente
 })
 export class TimelineComponent {
-  @Input() progress: number = 0; // Recibe el progreso calculado desde DashboardComponent
 }
