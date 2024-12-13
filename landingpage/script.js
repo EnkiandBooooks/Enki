456749c7bb4eea5367e9e2f183291b731c3966df
PolBuqueras <pol.buqueras.ext@inetum.com>
2024-12-13 11:57:43 +0100

    <feat> Navbar 1

--- landingpage/script.js
+++ landingpage/script.js
@@ -8,19 +8,21 @@ document.addEventListener("DOMContentLoaded", () => {
   // Seleccionamos el elemento canvas
 const canvas = document.getElementById('canvas1');
 
+canvas.style.transition = 'background 5s, color 5s, transform 5s';
+
 // Establecemos dos configuraciones de colores como objetos
 const gradient1 = {
-  '--gradient-color-1': '#1F9EA3', // Morado oscuro
-  '--gradient-color-2': '#F8BD97', // Morado claro
-  '--gradient-color-3': '#9E5428', // Morado intermedio
-  '--gradient-color-4': '#3B0102', // Morado muy oscuro
+  '--gradient-color-1': '#1F9EA3',
+  '--gradient-color-2': '#F8BD97', 
+  '--gradient-color-3': '#9E5428', 
+  '--gradient-color-4': '#3B0102',
 };
 
 const gradient2 = {
-  '--gradient-color-1': '#525252', // Verde oscuro
-  '--gradient-color-2': '#66576c', // Verde claro
-  '--gradient-color-3': '#d89df0', // Verde intermedio
-  '--gradient-color-4': '#36dce7', // Verde muy oscuro
+  '--gradient-color-1': '#525252',
+  '--gradient-color-2': '#66576c', 
+  '--gradient-color-3': '#d89df0',
+  '--gradient-color-4': '#36dce7',
 };
 
 // Función para aplicar un gradiente al elemento
@@ -43,45 +45,4 @@ setInterval(() => {
 
 // Aplicamos inicialmente el primer gradiente
 applyGradient(canvas, gradient1);
-
-  
-  /*
-  const frameContainer = document.createElement("div");
-  frameContainer.id = "animation-container";
-  document.body.appendChild(frameContainer);
-
-  const totalFrames = 50; // Número total de frames
-  const scrollSpeed = 100; // Velocidad de reproducción, más alto = más rápido
-  const frames = [];
-
-  // Carga dinámica de los SVGs
-  for (let i = 1; i <= totalFrames; i++) {
-    const img = document.createElement("img");
-    img.src = `frames/frame (${i}).png`;
-    img.classList.add("frame");
-    img.style.opacity = 0; // Ocultamos los frames inicialmente
-    frameContainer.appendChild(img);
-    frames.push(img);
-  }
-
-  // Inicializa GSAP
-  gsap.registerPlugin(ScrollTrigger);
-
-  const timeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: "#animation-container",
-      start: "top top",
-      end: `+=${(totalFrames / scrollSpeed) * 100}%`, // Ajusta la longitud del scroll según la velocidad
-      scrub: 1,
-      pin: true,
-    },
-  });
-
-  frames.forEach((frame, index) => {
-    const frameStart = (index * scrollSpeed) / totalFrames;
-    const frameEnd = ((index + 1) * scrollSpeed) / totalFrames;
-
-    timeline.to(frame, { opacity: 1, duration: 0, ease: "none" }, frameStart);
-    timeline.to(frame, { opacity: 0, duration: 0, ease: "none" }, frameEnd);
-  });*/
 });
